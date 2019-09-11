adb6890ec03d60d713e51010a888a63c4d37926c
David Chen <dchen@sightmachine.com>
2019-09-10 22:59:53 -0700

    init commit

---
+++ code.py
@@ -0,0 +1,19 @@
+# Why do we need to test prospective merges?
+""" 
+Let's say my goal is two things:
+1. Print x from 1 to 10.
+2. Empty x.
+
+The initial commit is going to miss part of #1 and #2.
+"""
+x = []
+
+def printOneToTenThenDelete():
+    x += [1,2,3]
+    x += [4,5,6]
+    x += [7,8,9]
+    
+    return x
+
+result = printOneToThenThenDelete()
+assert len(result) == 0
